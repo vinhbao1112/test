480ead57c8bd4fb7ebb435f5cf7ad34ed69e56b2
vinhbao1112 <nguyenvb320@gmail.com>
2022-12-26 14:52:44 +0700

    test

---
+++ js.js
@@ -0,0 +1,53 @@
+// Bài 1
+
+// var styles = ["Jazz","Blues"]
+
+// var a = styles.push("Rock-n-Roll")
+// console.log(styles)
+
+// var b = styles.splice(1,1,"Classics")
+// console.log(styles)
+
+// var c = styles.shift()
+// console.log(c)
+
+// var d = styles.unshift("Rap","Reggae")
+// console.log(styles)
+
+// Bài 2
+
+// var arr = [1,2,3,4,5]
+
+// var a = arr.join("-")
+// console.log(a)
+
+// var b = arr.unshift(0)
+// console.log(arr)
+
+// var c = arr.push(6,7)
+// console.log(arr)
+
+// var d = arr.splice(6,2)
+// console.log(arr)
+
+// var e = arr.splice(0,2)
+// console.log(arr)
+
+// var bao = [320]
+// var f = arr.concat(bao)
+// console.log(f)
+
+// Bài 3
+
+// const a = 'techc'
+// const b = [1, 2, 4, 0]
+
+// console.log(Array.isArray(a))
+// console.log(Array.isArray(b))
+
+// Bài 4
+
+// const a = [1, 2, 4, 0]
+
+// const b = a.map(a=>a)
+// console.log(b)
